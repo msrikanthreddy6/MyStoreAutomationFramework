a0941e9984e2a9d578653c5cc14a33470be87bf4
Ihor Pryyma <avtomat200@gmail.com>
2019-03-17 18:05:31 +0200

    small fix

--- src/test/java/com/mystore/qa/testcases/ui/AccountPageTest.java
+++ src/test/java/com/mystore/qa/testcases/ui/AccountPageTest.java
@@ -5,7 +5,7 @@ import com.mystore.qa.pages.AccountPage;
 import com.mystore.qa.pages.HomePage;
 import com.mystore.qa.pages.LoginPage;
 import com.mystore.qa.pages.TShirtsPage;
-import org.testng.Assert;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
